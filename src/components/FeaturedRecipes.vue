f7ef43e169bad9e2622bbc310c8e3c97e9e19f11
luantran <luantran.24hdev@gmail.com>
2022-12-06 17:57:15 +0700

    test travis

--- src/components/FeaturedRecipes.vue
+++ src/components/FeaturedRecipes.vue
@@ -42,6 +42,7 @@ export default {
     };
   },
   created() {
+    console.log('trst');
     window.document.title = `Open Drinks - Featured`;
     this.featuredList = recipes.getFeaturedRecipes();
   },

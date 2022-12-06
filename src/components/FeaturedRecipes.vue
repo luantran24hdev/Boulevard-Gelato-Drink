dfe6725e010c1854a1ba298d7c715463f42b69db
luantran <luantran.24hdev@gmail.com>
2022-12-06 17:16:41 +0700

    remove log

--- src/components/FeaturedRecipes.vue
+++ src/components/FeaturedRecipes.vue
@@ -42,7 +42,6 @@ export default {
     };
   },
   created() {
-    console.log('FEATURED_ITEM_SIZE',FEATURED_ITEM_SIZE);
     window.document.title = `Open Drinks - Featured`;
     this.featuredList = recipes.getFeaturedRecipes();
   },
